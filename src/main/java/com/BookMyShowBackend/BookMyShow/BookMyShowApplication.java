05aea20c3b526c88eff53c946f418fad3876cd61
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2023-01-21 18:43:39 +0530

    added swagger

--- src/main/java/com/BookMyShowBackend/BookMyShow/BookMyShowApplication.java
+++ src/main/java/com/BookMyShowBackend/BookMyShow/BookMyShowApplication.java
@@ -2,12 +2,20 @@ package com.BookMyShowBackend.BookMyShow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+
 public class BookMyShowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMyShowApplication.class, args);
 	}
+
+
+
+
 
 }
